b40b9492e465b74983e4090f828f295462da5090
nrobinson <nrobinson@transparent.com>
2016-02-15 20:18:17 +0000

    Importing ValidationError

--- r2dto_rdf/__init__.py
+++ r2dto_rdf/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
-from r2dto_rdf.serializer import RdflibNamespaceManager, RdfSerializer
+from r2dto_rdf.errors import ValidationError
 from r2dto_rdf.fields import RdfField, RdfIriField, RdfSetField, RdfObjectField, \
     RdfStringField
 from r2dto_rdf.mapping import create_rdf_serializer_from_r2dto_serializer
+from r2dto_rdf.serializer import RdflibNamespaceManager, RdfSerializer
